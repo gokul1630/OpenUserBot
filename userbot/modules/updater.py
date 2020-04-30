3b252e33665e9bf9573b398ca56f443e9c609bb6
gokul1630 <gokulgp163034@gmail.com>
2020-04-30 14:15:51 +0530

    fix updater.py for updates

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -80,7 +80,7 @@ async def upstream(ups):
         repo.heads.sql-extended.checkout(True)
 
     ac_br = repo.active_branch.name
-    if ac_br != 'sql-extended':
+    if ac_br != 'master':
         await ups.edit(
             f'**[UPDATER]:**` Looks like you are using your own custom branch ({ac_br}). '
             'in that case, Updater is unable to identify '
